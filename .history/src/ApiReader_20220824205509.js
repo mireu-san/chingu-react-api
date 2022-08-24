15e92f8a73f7d4716463e03debb8610de087ee0f
Chesed Kim <starmireu@gmail.com>
2022-08-24 20:55:40 +0900

    organised files

---
+++ .history/src/ApiReader_20220824205509.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="ApiReader">
+      This looks empty and fearful, but we can plan and make the feature we
+      aimed for.
+    </div>
+  );
+}
+
+export default App;
